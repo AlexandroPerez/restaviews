2b2db9a52f2d8e224184489a565eb9c4b86e5b85
Alexandro Perez <apgfriend@gmail.com>
2018-06-07 13:19:56 +0100

    Finish making changes to styles for small devices

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -124,14 +124,17 @@ fillReviewsHTML = (reviews = self.restaurant.reviews) => {
 createReviewHTML = (review) => {
   const li = document.createElement('li');
   const name = document.createElement('p');
+  name.className = "name";
   name.innerHTML = review.name;
   li.appendChild(name);
 
   const date = document.createElement('p');
+  date.className = "date";
   date.innerHTML = review.date;
   li.appendChild(date);
 
   const rating = document.createElement('p');
+  rating.className = "rating";
   rating.innerHTML = `Rating: ${review.rating}`;
   li.appendChild(rating);
 
